b1de684f444240826976ab1dcd1b9b245064e522
Sinelnicov Alexandru <alexandru.sinelnicov@gmail.com>
2020-05-23 16:02:11 +0300

    upload2

--- functions.php
+++ functions.php
@@ -16,7 +16,41 @@ function CloseCon($conn)
 $conn -> close();
 }
 
+function new_line($n,$p,$b){
+    $conn=OpenCon();
+    $sql="INSERT INTO `tabel` (`id`, `name`, `power`, `beats`) VALUES (NULL, '$n', '$p', '$b');";
+    mysqli_query( $conn, $sql );
+    CloseCon($conn); 
+ }
 
+function read(){
+    $conn=OpenCon();  
+    $sql="SELECT * FROM tabel ;";
+    $query = mysqli_query( $conn, $sql );
+    if(mysqli_num_rows($query) > 0){
+   while($row = $query->fetch_array()){
+      $rows[]=$row;
+   }
+   return $rows;
+   }
+   else return 0;
+ }
+
+ function update($id,$n,$p,$b){
+    $conn=OpenCon();
+    $sql="UPDATE tabel SET name='$n', power='$p', beats='$b' WHERE id=$id;";
+    mysqli_query( $conn, $sql );
+ 
+    CloseCon($conn);
+ }
+
+ function delete($id){
+    $conn=OpenCon();
+    $sql="DELETE FROM tabel WHERE id=$id;";
+    mysqli_query( $conn, $sql );
+ 
+    CloseCon($conn);
+ }
 
 
 ?>
